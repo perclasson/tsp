b64667ac1b691a304959144e86425b1063c3c299
Per Classon <perwclasson@gmail.com>
2013-12-04 12:15:41 +0100

    Removed TSP2

--- src/TSP.java
+++ src/TSP.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class TSP {
 	private double[][] coordinates;
@@ -50,7 +48,7 @@ public class TSP {
 			}
 			break;
 		}
-		return existingRoute;
+		return route;
 	}
 
 	private boolean isGain(Short[] existingRoute, int i, int k) {
